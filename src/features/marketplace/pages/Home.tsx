c1633bff30e0509eda30026bacb7814caca10442
warissah <warissahossain@gmail.com>
2025-10-21 21:40:44 -0700

    items
    added items can later be changed

--- src/features/marketplace/pages/Home.tsx
+++ src/features/marketplace/pages/Home.tsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+<<<<<<< Updated upstream
 import "./styles.css";
+=======
+import laptopImg from "../../../assets/laptop.jpeg"
+
+>>>>>>> Stashed changes
 
 function Home() {
   // holds user search
   const [searchTerm, setSearchTerm] = useState("");
 
+<<<<<<< Updated upstream
   // items
   const items = [
     //fake for now , API connects here for real data
@@ -39,6 +45,29 @@ function Home() {
   const filteredItems = items.filter((item) =>
     item.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
+=======
+    // holds user search
+    const [searchTerm, setSearchTerm] = useState("");
+
+
+    // items 
+    const items = [
+      { id: 1, name: "Laptop", price: "$500", image: "https://via.placeholder.com/100", postedBy: "Alice" },
+      { id: 2, name: "Headphones", price: "$40", image: "https://via.placeholder.com/100", postedBy: "Ryan" },
+      { id: 3, name: "Backpack", price: "$30", image: "https://via.placeholder.com/100", postedBy: "Sophie" },
+      { id: 4, name: "Camera", price: "$250", image: "https://via.placeholder.com/100", postedBy: "Daniel" },
+    ];
+
+
+    // filter to search
+  const filteredItems =
+    searchTerm.trim() === ""
+      ? items
+      : items.filter((item) =>
+          item.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+>>>>>>> Stashed changes
 
   return (
     <div>
@@ -59,6 +88,26 @@ function Home() {
       />
 
       {/* item cards */}
+<<<<<<< Updated upstream
+=======
+      <div className="items-wrapper">
+        <div className="item-container">
+          {filteredItems.length > 0 ? (
+            filteredItems.map((item) => (
+              <Link to={`/item/${item.id}`} key={item.id} className="item-card">
+                <img className="item-img" src={laptopImg} alt={item.name} />
+                <h3>{item.name}</h3>
+                <p>{item.price}</p>
+                <p className="posted-by">Posted by: {item.postedBy}</p>
+              </Link>
+            ))
+          ) : (
+            <p className="no-items">No items found 😕</p>
+          )}
+        </div>
+      </div>
+
+>>>>>>> Stashed changes
     </div>
   );
 }
